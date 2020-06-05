cd03592899ec0359cb8c3808f4ee54dabeada3b8
carlsberg-stack <developer.amitjaiswal@gmail.com>
2020-06-05 12:20:18 +0530

    Initial

--- simonix-base-code/src/main/java/com/carlsberg_stack/simonix_base_code/base/kisok/CarlsKisokActivity.java
+++ simonix-base-code/src/main/java/com/carlsberg_stack/simonix_base_code/base/kisok/CarlsKisokActivity.java
@@ -18,9 +18,9 @@ public abstract class CarlsKisokActivity extends CarlsActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mDpm = (DevicePolicyManager) getSystemService(Context.DEVICE_POLICY_SERVICE);
-        if (defaultPreference.getBoolean(getString(R.string.carls_kisok_mode), true)) {
+//        if (defaultPreference.getBoolean(getString(R.string.carls_kisok_mode), false)) {
             carls_enter_kisok_mode(carl_getPackageName());
-        }
+//        }
     }
 
     private void carls_enter_kisok_mode(String package_name) {
